692aff3cd2d8389dd3c4387b97d32c80b854338d
KerissaGit <kerissa525@gmail.com>
2025-04-08 17:25:01 -0400

    Began Models.py and Seed.py

--- server/models.py
+++ server/models.py
@@ -1,6 +1,69 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.orm import validates
 
 from config import db
 
-# Models go here!
+
+class Doctor(db.Model, SerializerMixin):
+    __tablename__ = 'doctors'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    specialty = db.Column(db.String)
+    age_of_care = db.Column(db.Integer)
+
+    # Add relationship
+
+
+    # serialize_rules = (,)
+
+
+
+class Child(db.Model, SerializerMixin):
+    __tablename__ = 'children'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    age = db.Column(db.Integer)
+    appointment = db.Column(db.DateTime)
+    parent_id = db.Column(db.Integer, db.ForeignKey('parents.id'), nullable = False)
+
+    # Add relationship
+
+
+    # serialize_rules = (,)
+
+
+
+class Parent(db.Model, SerializerMixin):
+    __tablename__ = 'parents'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    email = db.Column(db.String)
+    child_id = db.Column(db.Integer, db.ForeignKey('children.id'), nullable = False)
+
+    # Add relationship
+
+
+    # serialize_rules = (,)
+
+
+class Appointment(db.Model, SerializerMixin):
+    __tablename__ = 'appointment'
+
+    id = db.Column(db.Integer, primary_key=True)
+    child_id = db.Column(db.Integer, db.ForeignKey('children.id'), nullable = False)
+    doctor_id = db.Column(db.Integer, db.ForeignKey('doctors.id'), nullable = False)
+
+
+    # Add relationship
+
+
+    # serialize_rules = (,)
+
+
+
+
+
--- server/seed.py
+++ server/seed.py
@@ -10,6 +10,27 @@ from faker import Faker
 from app import app
 from models import db
 
+
+def create_doctors():
+    doctors = []
+    names = []
+    for _ in range(8):
+        name = fake.name()
+        while name in names:
+            name = fake.name()
+        names.append(name)
+
+        d = Doctor(
+            name = name,
+            specialty = fake.sentence(),
+            age_of_care = fake.int()
+        )
+        doctors.append(d)
+
+    return doctors
+
+
+
 if __name__ == '__main__':
     fake = Faker()
     with app.app_context():
