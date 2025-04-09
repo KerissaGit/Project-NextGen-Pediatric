66c144862888748a9a207d9db076b682bd382b09
KerissaGit <kerissa525@gmail.com>
2025-04-09 14:39:34 -0400

    Added App, Header and Footer

---
+++ client/src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Footer() {
+    return(
+        <footer className='footer'>
+            <h3>
+                NextGen Pediatrics
+            </h3>
+            <div>
+                <strong>Contact Information:</strong>
+                <h3>Phone: (555) 818-8008</h3>
+                <h3>Email: NextGenPediatrics@DocsHelpingKids.com</h3>
+                <h3>Address: P.Sherman, 42 Wallaby Way, Sydney</h3>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
